34f364e6a3dcf7998676cef84a53b5e7cbf7dcb8
Eugenio Pastoral <stonenobrein@gmail.com>
2020-04-26 17:46:55 +0800

    Improved like/unlike behavior

--- public/js/social.js
+++ public/js/social.js
@@ -130,12 +130,13 @@ $(document).ready(function() {
 
   //like post
   $('.photo-actions').on('click', '#unliked', function() {
+    var target = $(event.target);
     var post_id = ($(this).attr("class")).substring(1);
     $.post('/likepost', {post_id: post_id}, function(result) {
       if(result) {
-        $('#unlikedpic').attr("src", "img/heart2.png");
-        $('#unlikedpic').attr("id", "likedpic");
-        $('#unliked').attr("id", "liked");
+        target.attr("src", "img/heart2.png");
+        target.attr("id", "likedpic");
+        target.attr("id", "liked");
       }
       else {
         return;
@@ -145,12 +146,13 @@ $(document).ready(function() {
 
   //unlike post
   $('.photo-actions').on('click', '#liked', function() {
+    var target = $(event.target);
     var post_id = ($(this).attr("class")).substring(1);
     $.post('/unlikepost', {post_id: post_id}, function(result) {
       if(result) {
-        $('#likedpic').attr("src", "img/heart.png");
-        $('#likedpic').attr("id", "unlikedpic");
-        $('#liked').attr("id", "unliked");
+        target.attr("src", "img/heart.png");
+        target.attr("id", "unlikedpic");
+        target.attr("id", "unliked");
       }
       else {
         return;
